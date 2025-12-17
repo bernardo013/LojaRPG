0a20de7f73ac424463557d3f7590fd7ffdb950aa
Bernardo Mariné <bernardo.marine@villela.local>
2025-12-17 10:06:29 -0300

    lógica de adicionar e diminuir produtos  no carrinho
    lógica de adicionar e diminuir o produto do carrinho, e personalização dos produtos no carrinho

--- assets/scripts/cart.js
+++ assets/scripts/cart.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            const carrinho = document.querySelector('.container-detalhe')
+            const carrinho = document.querySelector('.container-detalhes-main-cart')
             const produtos = JSON.parse(xhr.responseText)
 
             console.log('p', produtos)
@@ -18,13 +18,26 @@ document.addEventListener('DOMContentLoaded', () => {
                 //some testa se algum elemento do array passa na condição
                 if (idProdutosCarrinho.find((idDoCarrinho) => idDoCarrinho == e.id)) {
                     console.log('entrou', e)
-                    carrinho.innerHTML += `
-                <nav class="detalhes-card-cart">
-                    <img src="/assets/images/quest.png" alt="" class="detalhes-img">
-                    <h3 class="detalhes-nome">${e.nome}</h3>
-                    <p class="detalhes-preco">${e.preco}</p>
-                    <p class="detalhes-descricao">aa</p>
-                </nav>
+                    carrinho.innerHTML += `             
+                        <main class="container-detalhes-main-cart">
+                            <article class="container-detalhes-cart">
+
+                                <div class="detalhes-info-cart">
+                                    <img src="/assets/images/quest.png" class="detalhes-img-cart">
+
+                                    <h3 class="detalhes-nome-cart">${e.nome}</h3>
+                                    <p class="detalhes-preco-cart">${e.preco}</p>
+                                    <p class="detalhes-descricao-cart">${e.descricao}</p>
+
+                                
+                                    <div class="controle-quantidade">
+                                        <button class="btn-quantidade-diminuir" onclick="diminuirQuantidade()">-</button>
+                                        <span class="quantidade">0</span>
+                                        <button class="btn-quantidade-aumentar" onclick="addQuantidade()">+</button>
+                                    </div>
+                                </div>
+                            </article>                        
+                        </main>
                 `}
             })
         }
@@ -32,3 +45,18 @@ document.addEventListener('DOMContentLoaded', () => {
     xhr.open('GET', '/produtos.json')
     xhr.send()
 })
+
+
+function addQuantidade() {
+        let quantidade = document.querySelector('.quantidade');
+        let valor = parseInt(quantidade.textContent);
+        valor++;
+        quantidade.textContent = valor;
+}
+function diminuirQuantidade() {}
+        let quantidade = document.querySelector('.quantidade');
+        let valor = parseInt(quantidade.textContent);
+        if (valor >= 1) {
+            valor--;
+            quantidade.textContent = valor;
+        }
